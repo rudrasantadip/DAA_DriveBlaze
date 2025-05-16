261d3393a0d200916602e33f27693562e084e241
rudrasantadip <santadip9@gmail.com>
2025-05-16 17:57:46 +0530

    two more problems

--- Day6/Main.java
+++ Day6/Main.java
@@ -4,7 +4,7 @@ class Node {
 
     public Node(int data) {
         this.data = data;
-        this.next=null;
+        this.next = null;
     }
 }
 
@@ -32,45 +32,143 @@ class LinkedList {
         temp.next = new Node(data);
     }
 
-
-    public void printList(){
-        if(head==null){
+    public void printList() {
+        if (head == null) {
             return;
         }
-        
-        Node temp=head;
-        while(temp!=null){
-            System.out.print(temp.data+" -> ");
-            temp=temp.next;
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
         }
     }
 
-    public Node middleNode(){
-
-        if(head==null){
-            return null; 
+    public static void printList(Node head) {
+        if (head == null) {
+            return;
         }
-        Node slow=head, fast=head;
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+    }
+
+    public Node middleNode() {
+
+        if (head == null) {
+            return null;
+        }
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
         return slow;
     }
 
+    public static Node addTwoNos(Node head1, Node head2) {
+        Node temp1 = head1, temp2 = head2;
+        Node dummy = new Node(-1);
+        Node dummyHead = dummy;
+        int carry = 0, sum = 0;
+
+        while (temp1 != null || temp2 != null) {
+            sum = carry;
+            if (temp1 != null) {
+                sum = sum + temp1.data;
+            }
+            if (temp2 != null) {
+                sum = sum + temp2.data;
+            }
+            carry = sum / 10; // msb
+            dummy.next = new Node(sum % 10);
+            dummy = dummy.next;
+
+            if (temp1 != null) {
+                temp1 = temp1.next;
+            }
+            if (temp2 != null) {
+                temp2 = temp2.next;
+            }
+        }
+        if (carry > 0) {
+            Node newNode = new Node(carry);
+            dummy.next = newNode;
+        }
+        return dummyHead.next;
+    }
+
+    public Node seperateOddEven() {
+
+        if (head == null) {
+            return null;
+        }
+        Node odd = head;
+        Node even = head.next;
+
+        Node evenHead = even;
+
+        while (even != null && even.next != null) {
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+
+            // shift
+            odd = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+        return head;
+    }
+
+    /*
+     * Given a singly linked list, delete the middle of the linked list.
+     * 
+     * Note:
+     * 
+     * If there are even nodes, then there would be two middle nodes, we need to
+     * delete the second middle element.
+     * If the input linked list has a single node, then it should return NULL.
+     */
+
+
+     public void deleteMiddle(){
+        if(head==null){
+            return;
+        }
+
+        Node slow = head,fast=head;
+        Node prev=null;
+
+        while (fast.next!=null && fast.next.next!=null){
+            fast=fast.next.next;
+            prev=slow;
+            slow=slow.next;
+        }
+        prev.next=slow.next;
+        slow.next=null;
+     }
 }
 
 public class Main {
 
     public static void main(String[] args) {
-        LinkedList ll = new LinkedList();
-        ll.insertAtEnd(1);
-        ll.insertAtEnd(2);
-        ll.insertAtEnd(3);
-        ll.insertAtEnd(4);
-    
-        ll.printList();
-    }
+        LinkedList l1 = new LinkedList();
+        l1.insertAtEnd(1);
+        l1.insertAtEnd(2);
+        l1.insertAtEnd(3);
+        l1.insertAtEnd(4);
+        l1.insertAtEnd(5);
 
+
+        l1.deleteMiddle();
+
+        l1.printList();
+
+
+    }
 
 }
